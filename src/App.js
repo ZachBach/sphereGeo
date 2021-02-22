c60b8493c942c4539933acff5898524406f22765
Zach Auerbach <auernachzach@gmail.com>
2021-02-22 11:21:38 -0800

    Sphere geometry for new react portfolio using three.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Canvas } from 'react-three-fiber';
+import Sphere from './components/Sphere';
+import Light from './components/Light';
+import KeyLight from './components/KeyLight';
+import Backdrop from './components/Backdrop';
+import GroundPlane from './components/GroundPlane';
+import FillLight from './components/FillLight';
+import RimLight from './components/RimLight';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  <Canvas>
+    <KeyLight brightness={10} color={"#ffbdf4e"} />
+    <FillLight brightness={2.6} color="#bdefff"/>
+    <Light brightness={10} color={"#eeeeee"}/>
+    <RimLight brightness={54} color="#fff"/>
+    <Sphere />
+    <Backdrop/>
+    <GroundPlane />
+  </Canvas>
+  )
 }
+
 
 export default App;
---
+++ src/components/Sphere.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+
+function Sphere() {
+    return (
+        <>
+       <mesh visible userData={{ test: "hello" }} position={[0, 0, 0]} castShadow>
+      <sphereGeometry attach="geometry" args={[1, 16, 16]} />
+      <meshStandardMaterial
+        attach="material"
+        color="dark"
+        transparent
+        roughness={0.1}
+        metalness={0.1}
+      />
+    </mesh>
+</>
+    )
+}
+
+export default Sphere
+
+
